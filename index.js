b79b38ec24ed32e2aa46681b138abdce33309bf4
neha.prakash <neha.prakash@akqa.com>
2024-09-11 13:23:41 +0530

    updated cors whitelist url

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ server.use(express.json())
 server.use(express.urlencoded({extended:true}))
 server.use(cookieParser())
 server.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://gfg-ekart.netlify.app",
     credentials: true
 }))
 conntectDB().then(() => {
