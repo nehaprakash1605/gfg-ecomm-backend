723ec1fea96f9414c9b0c2750604c7e976879af8
neha.prakash <neha.prakash@akqa.com>
2024-09-11 11:38:31 +0530

    change in cors url

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ server.use(express.json())
 server.use(express.urlencoded({extended:true}))
 server.use(cookieParser())
 server.use(cors({
-    origin:"http://localhost:5173/",
+    origin:"http://localhost:5173",
     credentials: true
 }))
 conntectDB().then(() => {
